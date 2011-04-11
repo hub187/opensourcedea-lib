2539a3049751527b89bd3885a8a56456aa36f8bb
hubert.virtos@gmail.com <hubert.virtos@gmail.com@5f36766c-0871-11df-bdae-5552c88b53c9>
2011-04-11 16:37:35 +0000

    Finished typing raw data (Data Matrix) for tests. Objectives test finished in TestCCRI test method.

--- UnitTests/tests/LibraryTest.java
+++ UnitTests/tests/LibraryTest.java
@@ -62,102 +62,102 @@ public class LibraryTest {
 		TestDataMatrix [0] [0] = 411.17;
 		TestDataMatrix [0] [1] = 83;
 		TestDataMatrix [0] [2] = 1877.18;
-		TestDataMatrix [0] [3] = 1345.276;
+		TestDataMatrix [0] [3] = 1345.27;
 		
 		TestDataMatrix [1] [0] = 1023.92;
 		TestDataMatrix [1] [1] = 84;
 		TestDataMatrix [1] [2] = 3529.32;
-		TestDataMatrix [1] [3] = 1345.276;		
+		TestDataMatrix [1] [3] = 2816.55;		
 		
 		TestDataMatrix [2] [0] = 1537.69;
 		TestDataMatrix [2] [1] = 148;
 		TestDataMatrix [2] [2] = 3923.24;
-		TestDataMatrix [2] [3] = 1007.528;
+		TestDataMatrix [2] [3] = 1007.52;
 		
 		TestDataMatrix [3] [0] = 361;
 		TestDataMatrix [3] [1] = 83;
 		TestDataMatrix [3] [2] = 1250.71;
-		TestDataMatrix [3] [3] = 151.9472;
+		TestDataMatrix [3] [3] = 151.94;
 		
 		TestDataMatrix [4] [0] = 492.78;
 		TestDataMatrix [4] [1] = 83;
 		TestDataMatrix [4] [2] = 2982.66;
-		TestDataMatrix [4] [3] = 2059.933;
+		TestDataMatrix [4] [3] = 2059.93;
 		
 		TestDataMatrix [5] [0] = 516.64;
 		TestDataMatrix [5] [1] = 83;
 		TestDataMatrix [5] [2] = 3778.31;
-		TestDataMatrix [5] [3] = 3396.074;
+		TestDataMatrix [5] [3] = 3396.07;
 		
 		TestDataMatrix [6] [0] = 259.3;
 		TestDataMatrix [6] [1] = 85;
 		TestDataMatrix [6] [2] = 3054.6;
-		TestDataMatrix [6] [3] = 2947.036;	
+		TestDataMatrix [6] [3] = 2947.03;	
 		
 		TestDataMatrix [7] [0] = 708.89;
 		TestDataMatrix [7] [1] = 84;
 		TestDataMatrix [7] [2] = 3237.17;
-		TestDataMatrix [7] [3] = 2940.285;	
+		TestDataMatrix [7] [3] = 2940.28;	
 
-		TestDataMatrix [8] [0] = 3;
-		TestDataMatrix [8] [1] = 7;
-		TestDataMatrix [8] [2] = 1;
-		TestDataMatrix [8] [3] = 1;	
+		TestDataMatrix [8] [0] = 266.28;
+		TestDataMatrix [8] [1] = 87;
+		TestDataMatrix [8] [2] = 1729.98;
+		TestDataMatrix [8] [3] = 1277.12;	
 		
-		TestDataMatrix [9] [0] = 3;
-		TestDataMatrix [9] [1] = 7;
-		TestDataMatrix [9] [2] = 1;
-		TestDataMatrix [9] [3] = 1;			
+		TestDataMatrix [9] [0] = 113.91;
+		TestDataMatrix [9] [1] = 85;
+		TestDataMatrix [9] [2] = 817.38;
+		TestDataMatrix [9] [3] = 193.87;			
 
-		TestDataMatrix [10] [0] = 3;
-		TestDataMatrix [10] [1] = 7;
-		TestDataMatrix [10] [2] = 1;
-		TestDataMatrix [10] [3] = 1;			
+		TestDataMatrix [10] [0] = 384.149;
+		TestDataMatrix [10] [1] = 65;
+		TestDataMatrix [10] [2] = 1409.55;
+		TestDataMatrix [10] [3] = 11.26;			
 
-		TestDataMatrix [11] [0] = 3;
-		TestDataMatrix [11] [1] = 7;
-		TestDataMatrix [11] [2] = 1;
-		TestDataMatrix [11] [3] = 1;			
+		TestDataMatrix [11] [0] = 208.91;
+		TestDataMatrix [11] [1] = 58;
+		TestDataMatrix [11] [2] = 2703.88;
+		TestDataMatrix [11] [3] = 42.02;			
 
-		TestDataMatrix [12] [0] = 3;
-		TestDataMatrix [12] [1] = 7;
-		TestDataMatrix [12] [2] = 1;
-		TestDataMatrix [12] [3] = 1;			
+		TestDataMatrix [12] [0] = 1174.17;
+		TestDataMatrix [12] [1] = 103;
+		TestDataMatrix [12] [2] = 2821.10;
+		TestDataMatrix [12] [3] = 474.20;			
 
-		TestDataMatrix [13] [0] = 3;
-		TestDataMatrix [13] [1] = 7;
-		TestDataMatrix [13] [2] = 1;
-		TestDataMatrix [13] [3] = 1;			
+		TestDataMatrix [13] [0] = 349.33;
+		TestDataMatrix [13] [1] = 75;
+		TestDataMatrix [13] [2] = 1190.92;
+		TestDataMatrix [13] [3] = 38.57;			
 
-		TestDataMatrix [14] [0] = 3;
-		TestDataMatrix [14] [1] = 7;
-		TestDataMatrix [14] [2] = 1;
-		TestDataMatrix [14] [3] = 1;			
+		TestDataMatrix [14] [0] = 327.25;
+		TestDataMatrix [14] [1] = 43;
+		TestDataMatrix [14] [2] = 504.83;
+		TestDataMatrix [14] [3] = 1918.92;			
 
-		TestDataMatrix [15] [0] = 3;
-		TestDataMatrix [15] [1] = 7;
-		TestDataMatrix [15] [2] = 1;
-		TestDataMatrix [15] [3] = 1;			
+		TestDataMatrix [15] [0] = 289.263;
+		TestDataMatrix [15] [1] = 39;
+		TestDataMatrix [15] [2] = 1199.11;
+		TestDataMatrix [15] [3] = 691.50;			
 
-		TestDataMatrix [16] [0] = 3;
-		TestDataMatrix [16] [1] = 7;
-		TestDataMatrix [16] [2] = 1;
-		TestDataMatrix [16] [3] = 1;			
+		TestDataMatrix [16] [0] = 99.16;
+		TestDataMatrix [16] [1] = 40;
+		TestDataMatrix [16] [2] = 730.44;
+		TestDataMatrix [16] [3] = 1641.83;			
 
-		TestDataMatrix [17] [0] = 3;
-		TestDataMatrix [17] [1] = 7;
-		TestDataMatrix [17] [2] = 1;
-		TestDataMatrix [17] [3] = 1;			
+		TestDataMatrix [17] [0] = 456.51;
+		TestDataMatrix [17] [1] = 40;
+		TestDataMatrix [17] [2] = 94.37;
+		TestDataMatrix [17] [3] = 1951.99;			
 
-		TestDataMatrix [18] [0] = 3;
-		TestDataMatrix [18] [1] = 7;
-		TestDataMatrix [18] [2] = 1;
-		TestDataMatrix [18] [3] = 1;			
+		TestDataMatrix [18] [0] = 229.11;
+		TestDataMatrix [18] [1] = 2;
+		TestDataMatrix [18] [2] = 147.57;
+		TestDataMatrix [18] [3] = 708.89;			
 
-		TestDataMatrix [19] [0] = 3;
-		TestDataMatrix [19] [1] = 7;
-		TestDataMatrix [19] [2] = 1;
-		TestDataMatrix [19] [3] = 1;			
+		TestDataMatrix [19] [0] = 59.65;
+		TestDataMatrix [19] [1] = 44;
+		TestDataMatrix [19] [2] = 152.98;
+		TestDataMatrix [19] [3] = 68.68;			
 	
 		
 		//Set up the variable types
@@ -183,26 +183,26 @@ public class LibraryTest {
 		DEASolution CCRISol = new DEASolution();
 		
 		double[] Objectives = new double[20];
-		Objectives[0] = 0.519434812021246;
-		Objectives[1] = 0.872473221578998;
-		Objectives[2] = 0.549046590401745;
-		Objectives[3] = 0.322936418671813;
-		Objectives[4] = 0.782831804168681;
+		Objectives[0] = 0.519434453047034;
+		Objectives[1] = 0.872474445374321;
+		Objectives[2] = 0.549048143682244;
+		Objectives[3] = 0.322936931532726;
+		Objectives[4] = 0.782832225762655;
 		Objectives[5] = 1;
 		Objectives[6] = 1;
-		Objectives[7] = 0.830850222668304;
-		Objectives[8] = 0.53968434894831;
-		Objectives[9] = 0.567195656859274;
-		Objectives[10] = 0.459596265794807;
+		Objectives[7] = 0.830850567998129;
+		Objectives[8] = 0.539682742929534;
+		Objectives[9] = 0.567189831743522;
+		Objectives[10] = 0.459594713883521;
 		Objectives[11] = 1;
-		Objectives[12] = 0.564402493712204;
-		Objectives[13] = 0.338734740348399;
-		Objectives[14] = 0.805611390795234;
-		Objectives[15] = 0.657217760429428;
+		Objectives[12] = 0.564403802355244;
+		Objectives[13] = 0.338735130146885;
+		Objectives[14] = 0.805598209487005;
+		Objectives[15] = 0.65721528448653;
 		Objectives[16] = 1;
-		Objectives[17] = 0.739090018036911;
+		Objectives[17] = 0.739084603920947;
 		Objectives[18] = 1;
-		Objectives[19] = 0.207059764006891;
+		Objectives[19] = 0.207079840963623;
 		
 		           
 		CCRISol.Objectives = Objectives;
@@ -233,7 +233,7 @@ public class LibraryTest {
 		double[] ObjectivesToCheck = tester.getObjectives();
 		
 		
-		assertArrayEquals(ObjectivesToCheck, CheckedSol.Objectives,0.0000001);	
+		assertArrayEquals(ObjectivesToCheck, CheckedSol.Objectives,0.000001);	
 	}
 	
 	
